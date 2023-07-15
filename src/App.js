627d5caceb3bf9b08a594a9c37ed1915537292c5
olger <olger@pc-olger>
2023-07-15 10:02:33 -0500

    creacion de la pagina en el aspecto visual

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+
+      <header className='header'>
+        <h1 className='header__title'>Remix Shop</h1>
+      </header>
+
+      <main className="container">
+        <div className="container__interaction">
+          <input className="container__search" type="text" placeholder="Búsqueda" autoComplete="off"/>
+          <a className="container__link container__link--login" href="#">Iniciar sesión</a>
+          <a className="container__link container__link--createAccount" href="#">Crear cuenta</a>
+        </div>
+
+        <div className="container__inside">
+          <button className="container__inside-btn container__inside-btn--price">Precio</button>
+          <button className="container__inside-btn container__inside-btn--categories">Categoría</button>
+        </div>
+
+        <section className='container__section'>
+          
+        </section>
+      </main>
+
+    <footer className='footer'>
+      <p>&copy; Remix. todos los derechos reservados</p>
+    </footer>
+
     </div>
   );
 }
